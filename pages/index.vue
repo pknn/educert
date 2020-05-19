62516aa2b46a10262750eb3d0dd0e285741c20dc
Pakanon Pantisawat <smith@Pakanons-MacBook-Pro.local>
2020-05-19 19:38:44 +0700

    Edit login

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <div v-if="role === 'officer'"></div>
+    <div v-else-if="role === 'student'"></div>
+    <div v-else></div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      role: this.$auth.user.role
+    }
+  }
+}
 </script>
