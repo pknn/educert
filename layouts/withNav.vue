2a3e1edb10396694d62fbdcc34bd829c48c8b2e4
Pakanon Pantisawat <smith@Pakanons-MacBook-Pro.local>
2020-05-21 01:03:19 +0700

    Clean up

--- layouts/withNav.vue
+++ layouts/withNav.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="px-2 py-4 bg-blue-300 inset-t-0 flex">
+    <div class="px-2 py-4 bg-blue-300 inset-t-0 flex shadow">
       <div class="flex-1" />
       <div>
         <button
@@ -38,40 +38,10 @@ html {
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
 }
-
 *,
 *:before,
 *:after {
   box-sizing: border-box;
   margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
 }
 </style>
