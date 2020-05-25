1e99d0b3c0124c7a37db6e06f8c0ccfca1566156
Pakanon Pantisawat <smith@Pakanons-MacBook-Pro.local>
2020-05-25 19:45:47 +0700

    FINISH

--- migration/getVC.sh
+++ migration/getVC.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-psql -U educert -d educert -c "select verification_code from pending_users where public_address='$1'"
+psql -U educert -d educert -c "select verification_code from pending_users where public_address=LOWER('$1')"
